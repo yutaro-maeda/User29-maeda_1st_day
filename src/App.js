18c43239881d612b6491a50be187b81fee0076c0
yutaro-maeda <yutaro-maeda@nec.com>
2025-06-06 17:22:27 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
-const API_ENDPOINT1 = '【API Gateway Lambda1のエンドポイント】';
-const API_ENDPOINT2 = '【API Gateway Lambda2のエンドポイント】';
+const API_ENDPOINT1 = '/User29_maeda_ms-message_1';
+const API_ENDPOINT2 = '/User29_maeda_ms-message_2';
 
 function App() {
   const [result, setResult] = useState('結果がここに表示されます');
